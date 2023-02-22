a759147558d8a1abec6f8c07c56197afd4052167
BipinL <104549520+BipinL@users.noreply.github.com>
2023-02-23 00:37:55 +0545

    student details phonished

--- lib/view/student_details.dart
+++ lib/view/student_details.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
 import 'package:school_project/util/appcolor.dart';
 
 class StudentDetailsView extends StatelessWidget {
@@ -34,20 +35,26 @@ class StudentDetailsView extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.fromLTRB(8, 28, 8, 0),
                           child: Column(
-                            children: const [
+                            children: [
                               Text(
                                 "Mr.Alex jhon",
                                 style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold),
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                    color: AppColor.kprimary),
                               ),
                               SizedBox(
                                 height: 5,
                               ),
-                              Text("Roll number:"),
+                              Text("Roll number:",
+                                  style: TextStyle(color: AppColor.kprimary)),
                               SizedBox(
                                 height: 5,
                               ),
-                              Text("PUR076BEI008")
+                              Text(
+                                "PUR076BEI008",
+                                style: TextStyle(color: AppColor.kprimary),
+                              )
                             ],
                           ),
                         ),
@@ -71,6 +78,152 @@ class StudentDetailsView extends StatelessWidget {
                     ),
                   ),
                 ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Container(
+                    height: 450,
+                    width: Get.size.width,
+                    color: AppColor.kprimary,
+                    child: SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "Student Details",
+                              style: TextStyle(
+                                  color: AppColor.ksecondary, fontSize: 24),
+                            ),
+                          ),
+                          Divider(
+                            color: AppColor.ksecondary,
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.calendar_month,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "Date of birth",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "2054-2-32",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.person,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "Gender",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "male",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.bloodtype,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "Bloode",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "o+",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.person,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "Fathers Name",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "Hari Prasad ",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.work,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "Occupation",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "Teacher",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.phone,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "contact",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "9359334",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.location_city,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "Address",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "Dharan",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          ),
+                          ListTile(
+                            leading: Icon(
+                              Icons.meeting_room,
+                              color: AppColor.ksecondary,
+                            ),
+                            title: Text(
+                              "class",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                            trailing: Text(
+                              "20",
+                              style: TextStyle(color: AppColor.ksecondary),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              MaterialButton(
+                onPressed: () {},
+                child:
+                    Text("Logout", style: TextStyle(color: AppColor.kprimary)),
+                color: AppColor.ksecondary,
               )
             ],
           ),
